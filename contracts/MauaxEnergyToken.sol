fd9f5684d070c94fbf8bb11259fbdaf3f7fadcd2
scoobiii <sobrinhosj@gmail.com>
2025-07-31 11:48:39 -0300

    Create MauaxEnergyToken.sol

---
+++ contracts/MauaxEnergyToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/**
+ * @title MAUAX Energy Token
+ * @dev Token de utilidade (MAUAX-E) lastreado em energia real (RWA).
+ * 1 token = 1 MWh de energia gerada. A emissão é controlada.
+ */
+contract MauaxEnergyToken is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("MAUAX Energy Token", "MAUAX-E") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}

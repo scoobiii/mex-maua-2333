1f8029e71af7d4308fc48fe5be77a094e7685fb5
scoobiii <sobrinhosj@gmail.com>
2025-08-01 12:13:10 -0300

    Update MauaxFoundersNFT.sol
    erc20 - > erc 721

--- contracts/MauaxFoundersNFT.sol
+++ contracts/MauaxFoundersNFT.sol
@@ -1,48 +1,58 @@
-# Placeholder para MauaxFoundersNFT.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 /**
- * @title MAUAX Utility Token
- * @author GOS3 Team
- * @notice Este contrato gerencia o MAUAX-C, a criptomoeda de utilidade fungível (ERC-20)
- * do ecossistema MAUAX.
- * @dev A propriedade do contrato (ownership) será detida pela Gnosis Safe Multisig do Consórcio
- * e, futuramente, transferida para a MAUAX DAO. Apenas o "dono" pode mintar novos tokens,
- * seguindo o cronograma de emissão (vesting) definido no tokenomics do projeto.
+ * @title MAUAX Governance Token (MAUAX-G)
+ * @author GOS3 Team (Claude, Grok, GPT, DeepSeek, Qwen, Gemini, Manus)
+ * @dev Em colaboração com o Consórcio MEXX.
+ * @notice Contrato para os NFTs de governança (MAUAX-G) da MAUAX DAO.
+ * @dev Implementa o padrão ERC-721. Apenas 100 NFTs ("Founder's Edition") podem ser criados.
+ * A propriedade do contrato deve ser transferida para a Gnosis Safe da DAO.
+ * Utiliza ERC721Enumerable para permitir a contagem de tokens de governança on-chain.
+ * Versão: 3.0
+ * Data: 29 de Julho de 2025
  */
-contract MauaxUtilityToken is ERC20, ERC20Burnable, Ownable {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MauaxFoundersNFT is ERC721, ERC721Enumerable, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    uint256 public constant MAX_SUPPLY = 100;
+
+    constructor() ERC721("MAUAX Governance Token", "MAUAX-G") Ownable(msg.sender) {}
 
     /**
-     * @dev O construtor define o nome e o símbolo do token.
-     * NENHUM token é criado na implantação; eles serão mintados conforme o cronograma.
+     * @notice Cria (minta) um novo NFT de governança e o atribui a um endereço.
+     * @dev Apenas o dono do contrato (Gnosis Safe / DAO) pode chamar esta função.
+     * A emissão é limitada pela constante MAX_SUPPLY.
+     * @param to O endereço que receberá o novo NFT.
      */
-    constructor() ERC20("MAUAX Utility Token", "MAUAX-C") Ownable(msg.sender) {
-        // O msg.sender (deployer) se torna o dono inicial.
-        // A propriedade deve ser transferida para a Gnosis Safe imediatamente.
+    function safeMint(address to) external onlyOwner {
+        require(_tokenIdCounter.current() < MAX_SUPPLY, "MAUAX-G: Max supply reached");
+        
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(to, tokenId);
     }
 
-    /**
-     * @notice Cria (minta) uma nova quantidade de tokens e os atribui a um endereço.
-     * @dev Apenas o dono do contrato (a Gnosis Safe / DAO) pode chamar esta função.
-     * Esta função será usada para executar o cronograma de emissão (vesting) das Fases I e II.
-     * @param to O endereço que receberá os novos tokens.
-     * @param amount A quantidade de tokens a ser criada (em sua menor unidade, ex: com 18 decimais).
-     */
-    function mint(address to, uint256 amount) external onlyOwner {
-        _mint(to, amount);
+    // As funções a seguir são overrides necessários devido à herança de ERC721 e ERC721Enumerable.
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    /**
-     * @notice Transfere a propriedade do contrato para um novo endereço.
-     * @dev Função CRÍTICA para transferir o controle administrativo para a Gnosis Safe Multisig
-     * e, eventualmente, para o contrato de governança da MAUAX DAO.
-     */
-    function transferContractOwnership(address newOwner) external onlyOwner {
-        transferOwnership(newOwner);
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
